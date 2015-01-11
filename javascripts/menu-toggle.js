c79ed5e43d9ed738ddafbafda221954c6d31453b
Pufu Andrei Cristian <a.c_pufu@yahoo.com>
2015-01-11 17:03:06 +0200

    ff

--- javascripts/menu-toggle.js
+++ javascripts/menu-toggle.js
@@ -1,6 +1,6 @@
 var main = function() {
 	$(".menu-btn").click(function(){
-		$(this).setAttribute('style','background-color:#3A5795');
+		$(this).setAttribute('style','background-color:#3A5795;');
 	});
 }
 $(document).ready(main);
