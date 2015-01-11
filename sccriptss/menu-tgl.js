80569cdb9c7397d107d8807d61d218ce6e48cd85
Pufu Andrei Cristian <a.c_pufu@yahoo.com>
2015-01-11 17:52:41 +0200

    fd

--- sccriptss/menu-tgl.js
+++ sccriptss/menu-tgl.js
@@ -1,5 +1,7 @@
 var main = function() {
 	$(".nav a").click(function(){
+		$('.nav a').css('background-color','#ffffff');
+		$('.nav a').css('color','#5a5a5a');
 	    $(this).css('background-color','#3A5795');
 	    $(this).css('color','#ffffff');
 	});
