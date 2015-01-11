fd1e14076ef91c1e717c36420c5a99d61e359744
Pufu Andrei Cristian <a.c_pufu@yahoo.com>
2015-01-11 20:31:58 +0200

    hah

--- sccriptss/menu-tgl.js
+++ sccriptss/menu-tgl.js
@@ -12,7 +12,7 @@ var main = function() {
 		$('#mainmenu a').css('background-color','#ffffff');
 		$('#mainmenu a').css('color','#5a5a5a');
 		$('#secmenu ul').empty();
-	    $('#menunr2').append('<li><a>Noutăți</a></li><li><a>Echipa managerială</a></li><li><a>Cadru profesoral</a></li><li><a>Comisii de lucru</a></li><li><a>Oferta școlară</a></li>');
+	    $('#menunr2').append('<li><a>Descriere</a></li><li><a>Misiune și viziune</a></li><li><a>Uniforma</a></li><li><a>Galerie</a></li>');
 	    $('#secmenu').css('border-bottom', '1px solid #aeaeae;');
 	    $(this).css('background-color','#3A5795');
 	    $(this).css('color','#ffffff');
@@ -21,7 +21,7 @@ var main = function() {
 		$('#mainmenu a').css('background-color','#ffffff');
 		$('#mainmenu a').css('color','#5a5a5a');
 		$('#secmenu ul').empty();
-	    $('#menunr2').append('<li><a>Noutăți</a></li><li><a>Echipa managerială</a></li><li><a>Cadru profesoral</a></li><li><a>Comisii de lucru</a></li><li><a>Oferta școlară</a></li>');
+	    $('#menunr2').append('<li><a>Componența comisiei</a></li><li><a>Regulament</a></li><li><a>Proceduri</a></li><li><a>Plan Operațional</a></li>');
 	    $('#secmenu').css('border-bottom', '1px solid #aeaeae;');
 	    $(this).css('background-color','#3A5795');
 	    $(this).css('color','#ffffff');
@@ -30,7 +30,7 @@ var main = function() {
 		$('#mainmenu a').css('background-color','#ffffff');
 		$('#mainmenu a').css('color','#5a5a5a');
 		$('#secmenu ul').empty();
-	    $('#menunr4').append('<li><a>Noutăți</a></li><li><a>Echipa managerială</a></li><li><a>Cadru profesoral</a></li><li><a>Comisii de lucru</a></li><li><a>Oferta școlară</a></li>');
+	    $('#menunr4').append('<li><a>Proiecte și programe</a></li><li><a>Ziua colegiului</a></li><li><a>Cercuri </a></li><li><a>Festivaluri</a></li><li><a>Festivitate de absolvire</a></li>');
 	    $('#secmenu').css('border-bottom', '1px solid #aeaeae;');
 	    $(this).css('background-color','#3A5795');
 	    $(this).css('color','#ffffff');
@@ -39,7 +39,7 @@ var main = function() {
 		$('#mainmenu a').css('background-color','#ffffff');
 		$('#mainmenu a').css('color','#5a5a5a');
 		$('#secmenu ul').empty();
-	    $('#menunr5').append('<li><a>Noutăți</a></li><li><a>Echipa managerială</a></li><li><a>Cadru profesoral</a></li><li><a>Comisii de lucru</a></li><li><a>Oferta școlară</a></li>');
+	    $('#menunr5').append('<li><a>Contact</a></li><li><a>Orar</a></li><li><a>EDU</a></li><li><a>ISJVN</a></li><li><a>CCDF</a></li>');
 	    $('#secmenu').css('border-bottom', '1px solid #aeaeae;');
 	    $(this).css('background-color','#3A5795');
 	    $(this).css('color','#ffffff');
