7865ab1ffdda6ada08845024bc18408d2b51a3b4
Pufu Andrei Cristian <a.c_pufu@yahoo.com>
2015-01-11 20:19:43 +0200

    hellyeah

--- sccriptss/menu-tgl.js
+++ sccriptss/menu-tgl.js
@@ -10,5 +10,51 @@ var main = function() {
 	});
 };
 
+var main = function() {
+	$("#isteric").click(function(){
+		$('#mainmenu a').css('background-color','#ffffff');
+		$('#mainmenu a').css('color','#5a5a5a');
+		$('#secmenu ul').empty();
+	    $('#menunr2').append('<li><a>Noutăți</a></li><li><a>Echipa managerială</a></li><li><a>Cadru profesoral</a></li><li><a>Comisii de lucru</a></li><li><a>Oferta școlară</a></li>');
+	    $('#secmenu').css('border-bottom', '1px solid #aeaeae;')
+	    $(this).css('background-color','#3A5795');
+	    $(this).css('color','#ffffff');
+	});
+};
+
+var main = function() {
+	$("#ceacpag").click(function(){
+		$('#mainmenu a').css('background-color','#ffffff');
+		$('#mainmenu a').css('color','#5a5a5a');
+		$('#secmenu ul').empty();
+	    $('#menunr3').append('<li><a>Noutăți</a></li><li><a>Echipa managerială</a></li><li><a>Cadru profesoral</a></li><li><a>Comisii de lucru</a></li><li><a>Oferta școlară</a></li>');
+	    $('#secmenu').css('border-bottom', '1px solid #aeaeae;')
+	    $(this).css('background-color','#3A5795');
+	    $(this).css('color','#ffffff');
+	});
+};
+var main = function() {
+	$("#actiext").click(function(){
+		$('#mainmenu a').css('background-color','#ffffff');
+		$('#mainmenu a').css('color','#5a5a5a');
+		$('#secmenu ul').empty();
+	    $('#menunr4').append('<li><a>Noutăți</a></li><li><a>Echipa managerială</a></li><li><a>Cadru profesoral</a></li><li><a>Comisii de lucru</a></li><li><a>Oferta școlară</a></li>');
+	    $('#secmenu').css('border-bottom', '1px solid #aeaeae;')
+	    $(this).css('background-color','#3A5795');
+	    $(this).css('color','#ffffff');
+	});
+};
+
+var main = function() {
+	$("#utilepg").click(function(){
+		$('#mainmenu a').css('background-color','#ffffff');
+		$('#mainmenu a').css('color','#5a5a5a');
+		$('#secmenu ul').empty();
+	    $('#menunr5').append('<li><a>Noutăți</a></li><li><a>Echipa managerială</a></li><li><a>Cadru profesoral</a></li><li><a>Comisii de lucru</a></li><li><a>Oferta școlară</a></li>');
+	    $('#secmenu').css('border-bottom', '1px solid #aeaeae;')
+	    $(this).css('background-color','#3A5795');
+	    $(this).css('color','#ffffff');
+	});
+};
 
 $(document).ready(main);
