47e1b7638dac849a9df37f9298b7780d7efa7b72
Pufu Andrei Cristian <a.c_pufu@yahoo.com>
2015-01-11 20:40:31 +0200

    s

--- sccriptss/menu-tgl.js
+++ sccriptss/menu-tgl.js
@@ -4,7 +4,7 @@ var main = function() {
 		$('#mainmenu a').css('color','#5a5a5a');
 		$('#secmenu ul').empty();
 	    $('#menunr1').append('<li><a>Noutăți</a></li><li><a>Echipa managerială</a></li><li><a>Cadru profesoral</a></li><li><a>Comisii de lucru</a></li><li><a>Oferta școlară</a></li>');
-	    $('#secmenu').css('border-bottom', '1px ',' solid #aeaeae');
+	    $('#secmenu').css('border-bottom', '1px solid #aeaeae');
 	    $(this).css('background-color','#3A5795');
 	    $(this).css('color','#ffffff');
 	});
