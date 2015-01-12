16e2d465fcf832721cc921194222c93cbe27c5c6
Pufu Andrei Cristian <a.c_pufu@yahoo.com>
2015-01-12 18:23:34 +0200

    s

--- sccriptss/menu-tgl.js
+++ sccriptss/menu-tgl.js
@@ -1,24 +1,24 @@
 var main = function() {
 	$('#secmenu ul').hide();
 	$("#pagprin").click(function(){
-		$('#mainmenu a').removeClass('active');
+		$('#mainmenu a').removeClass('menubtnactive');
 
 		$(this).addClass('active');
 	});
 	$("#isteric").click(function(){
-		$('#mainmenu a').removeClass('active');
+		$('#mainmenu a').removeClass('menubtnactive');
 		$(this).addClass('active');
 	});
 	$("#ceacpag").click(function(){
-		$('#mainmenu a').removeClass('active');
+		$('#mainmenu a').removeClass('menubtnactive');
 		$(this).addClass('active');
 	});
 	$("#actiext").click(function(){
-		$('#mainmenu a').removeClass('active');
+		$('#mainmenu a').removeClass('menubtnactive');
 		$(this).addClass('active');
 	});
 	$("#utilepg").click(function(){
-		$('#mainmenu a').removeClass('active');
+		$('#mainmenu a').removeClass('menubtnactive');
 		$(this).addClass('active');
 	});	
 	
