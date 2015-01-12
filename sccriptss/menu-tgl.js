4b5d52fe193b5d202b5da60e841541dc74f792d1
Pufu Andrei Cristian <a.c_pufu@yahoo.com>
2015-01-12 18:46:40 +0200

    s

--- sccriptss/menu-tgl.js
+++ sccriptss/menu-tgl.js
@@ -3,31 +3,31 @@ var main = function() {
 		$('#mainmenu a').removeClass('menubtnactive');
 		$('#secmenu ul').hide();
 		$(this).addClass('menubtnactive');
-		$('#menu1').show();
+		$('#menu1').css('display','inherit');
 	});
 	$("#isteric").click(function(){
 		$('#mainmenu a').removeClass('menubtnactive');
 		$('#secmenu ul').hide();
 		$(this).addClass('menubtnactive');
-		$('#menu2').show();
+		$('#menu2').css('display','inherit');
 	});
 	$("#ceacpag").click(function(){
 		$('#mainmenu a').removeClass('menubtnactive');
 		$('#secmenu ul').hide();
 		$(this).addClass('menubtnactive');
-		$('#menu3').show();
+		$('#menu3').css('display','inherit');
 	});
 	$("#actiext").click(function(){
 		$('#mainmenu a').removeClass('menubtnactive');
 		$('#secmenu ul').hide();
 		$(this).addClass('menubtnactive');
-		$('#menu4').show();
+		$('#menu4').css('display','inherit');
 	});
 	$("#utilepg").click(function(){
 		$('#mainmenu a').removeClass('menubtnactive');
 		$('#secmenu ul').hide();
 		$(this).addClass('menubtnactive');
-		$('#menu5').show();
+		$('#menu5').css('display','inherit');
 	});	
 	
 };
