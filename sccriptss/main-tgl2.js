e0544cbba05cf2fbab1fa3d6937901e32b83ad4f
Pufu Andrei Cristian <a.c_pufu@yahoo.com>
2015-01-12 17:27:33 +0200

    s

--- sccriptss/main-tgl2.js
+++
@@ -1,7 +0,0 @@
-var main = function() {
-	$("#menunr1 a").click(function(){
-			$('#menunr1').empty();
-		    $('#secmenu').css('border-bottom', '0px solid #aeaeae');
-		    $('#mainmenu').css('border-bottom', '1px solid #aeaeae');
-		});
-};
