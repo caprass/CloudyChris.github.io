56a2a2755c60e827cb422074b5557749c373e142
Pufu Andrei Cristian <a.c_pufu@yahoo.com>
2015-01-12 16:48:02 +0200

    s

--- sccriptss/menu-tgl.js
+++ sccriptss/menu-tgl.js
@@ -3,7 +3,7 @@ var main = function() {
 		$('#mainmenu a').css('background-color','#ffffff');
 		$('#mainmenu a').css('color','#5a5a5a');
 		$('#secmenu ul').empty();
-	    $('#menunr1').append('<li><a href="#" >Noutăți</a></li><li><a href="#">Echipa managerială</a></li><li><a href="#">Cadru profesoral</a></li><li><a href="#" >Comisii de lucru</a></li><li><a href="#">Oferta școlară</a></li>');
+	    $('#menunr1').append('<li><a id="m11" href="#" >Noutăți</a></li>	<li><a id="m12" href="#">Echipa managerială</a></li>		<li><a id="m13" href="#">Cadru profesoral</a></li>		<li><a id="m14" href="#" >Comisii de lucru</a></li>		<li><a id="m15" href="#">Oferta școlară</a></li>');
 	    $('#secmenu').css('border-bottom', '1px solid #aeaeae');
 	    $('#mainmenu').css('border-bottom', '0px solid #aeaeae');
 	    $(this).css('background-color','#3A5795');
@@ -13,7 +13,7 @@ var main = function() {
 		$('#mainmenu a').css('background-color','#ffffff');
 		$('#mainmenu a').css('color','#5a5a5a');
 		$('#secmenu ul').empty();
-	    $('#menunr1').append('<li><a href="#">Descriere</a></li><li><a href="#">Misiune și viziune</a></li><li><a href="#">Uniforma</a></li><li><a href="#">Galerie</a></li>');
+	    $('#menunr1').append('<li><a id="m21" href="#">Descriere</a></li>		<li><a id="m22" href="#">Misiune și viziune</a></li>		<li><a id="m23" href="#">Uniforma</a></li>		<li><a id="m24" href="#">Galerie</a></li>');
 	    $('#secmenu').css('border-bottom', '1px solid #aeaeae');
 	    $('#mainmenu').css('border-bottom', '0px solid #aeaeae');
 	    $(this).css('background-color','#3A5795');
@@ -23,7 +23,7 @@ var main = function() {
 		$('#mainmenu a').css('background-color','#ffffff');
 		$('#mainmenu a').css('color','#5a5a5a');
 		$('#secmenu ul').empty();
-	    $('#menunr1').append('<li><a href="#">Componența comisiei</a></li><li><a href="#">Regulament</a></li><li><a href="#">Proceduri</a></li><li><a href="#">Plan Operațional</a></li>');
+	    $('#menunr1').append('<li><a id="m31" href="#">Componența comisiei</a></li>		<li><a id="m32" href="#">Regulament</a></li>		<li><a id="m33" href="#">Proceduri</a></li>		<li><a id="m34" href="#">Plan Operațional</a></li>');
 	    $('#secmenu').css('border-bottom', '1px solid #aeaeae');
 	    $('#mainmenu').css('border-bottom', '0px solid #aeaeae');
 	    $(this).css('background-color','#3A5795');
@@ -33,7 +33,7 @@ var main = function() {
 		$('#mainmenu a').css('background-color','#ffffff');
 		$('#mainmenu a').css('color','#5a5a5a');
 		$('#secmenu ul').empty();
-	    $('#menunr1').append('<li><a href="#">Proiecte și programe</a></li><li><a href="#">Ziua colegiului</a></li><li><a href="#">Cercuri </a></li><li><a href="#">Festivaluri</a></li><li><a href="#">Festivitate de absolvire</a></li>');
+	    $('#menunr1').append('<li><a id="m41" href="#">Proiecte și programe</a></li>		<li><a id="m42" href="#">Ziua colegiului</a></li>		<li><a id="m43" href="#">Cercuri </a></li>		<li><a id="m44" href="#">Festivaluri</a></li>		<li><a id="m45" href="#">Festivitate de absolvire</a></li>');
 	    $('#secmenu').css('border-bottom', '1px solid #aeaeae');
 	    $('#mainmenu').css('border-bottom', '0px solid #aeaeae');
 	    $(this).css('background-color','#3A5795');
@@ -43,7 +43,7 @@ var main = function() {
 		$('#mainmenu a').css('background-color','#ffffff');
 		$('#mainmenu a').css('color','#5a5a5a');
 		$('#secmenu ul').empty();
-	    $('#menunr1').append('<li><a href="#">Contact</a></li><li><a href="#">Orar</a></li><li><a href="#">EDU</a></li><li><a href="#">ISJVN</a></li><li><a href="#">CCDF</a></li>');
+	    $('#menunr1').append('<li><a id="m51" href="#">Contact</a></li>		<li><a id="m52" href="#">Orar</a></li>			<li><a id="m53" href="#">EDU</a></li>		<li><a id="m54" href="#">ISJVN</a></li>		<li><a id="m55" href="#">CCDF</a></li>');
 	    $('#secmenu').css('border-bottom', '1px solid #aeaeae');
 	    $('#mainmenu').css('border-bottom', '0px solid #aeaeae');
 	    $(this).css('background-color','#3A5795');
