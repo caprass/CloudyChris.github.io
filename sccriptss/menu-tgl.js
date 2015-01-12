19c675303ac91331940902897692d3969f251693
Pufu Andrei Cristian <a.c_pufu@yahoo.com>
2015-01-12 16:26:16 +0200

    s

--- sccriptss/menu-tgl.js
+++ sccriptss/menu-tgl.js
@@ -13,11 +13,7 @@ var main = function() {
 		$('#mainmenu a').css('background-color','#ffffff');
 		$('#mainmenu a').css('color','#5a5a5a');
 		$('#secmenu ul').empty();
-	    $('#menunr1').append('
-	    	<li><a href="#">Descriere</a></li>
-	    	<li><a href="#">Misiune și viziune</a></li>
-	    	<li><a href="#">Uniforma</a></li>
-	    	<li><a href="#">Galerie</a></li>');
+	    $('#menunr1').append('<li><a href="#">Descriere</a></li><li><a href="#">Misiune și viziune</a></li><li><a href="#">Uniforma</a></li><li><a href="#">Galerie</a></li>');
 	    $('#secmenu').css('border-bottom', '1px solid #aeaeae');
 	    $('#mainmenu').css('border-bottom', '0px solid #aeaeae');
 	    $(this).css('background-color','#3A5795');
@@ -27,11 +23,7 @@ var main = function() {
 		$('#mainmenu a').css('background-color','#ffffff');
 		$('#mainmenu a').css('color','#5a5a5a');
 		$('#secmenu ul').empty();
-	    $('#menunr1').append('
-	    	<li><a href="#">Componența comisiei</a></li>
-	    	<li><a href="#">Regulament</a></li>
-	    	<li><a href="#">Proceduri</a></li>
-	    	<li><a href="#">Plan Operațional</a></li>');
+	    $('#menunr1').append('<li><a href="#">Componența comisiei</a></li><li><a href="#">Regulament</a></li><li><a href="#">Proceduri</a></li><li><a href="#">Plan Operațional</a></li>');
 	    $('#secmenu').css('border-bottom', '1px solid #aeaeae');
 	    $('#mainmenu').css('border-bottom', '0px solid #aeaeae');
 	    $(this).css('background-color','#3A5795');
@@ -41,12 +33,7 @@ var main = function() {
 		$('#mainmenu a').css('background-color','#ffffff');
 		$('#mainmenu a').css('color','#5a5a5a');
 		$('#secmenu ul').empty();
-	    $('#menunr1').append('
-	    	<li><a href="#">Proiecte și programe</a></li>
-	    	<li><a href="#">Ziua colegiului</a></li>
-	    	<li><a href="#">Cercuri </a></li>
-	    	<li><a href="#">Festivaluri</a></li>
-	    	<li><a href="#">Festivitate de absolvire</a></li>');
+	    $('#menunr1').append('<li><a href="#">Proiecte și programe</a></li><li><a href="#">Ziua colegiului</a></li><li><a href="#">Cercuri </a></li><li><a href="#">Festivaluri</a></li><li><a href="#">Festivitate de absolvire</a></li>');
 	    $('#secmenu').css('border-bottom', '1px solid #aeaeae');
 	    $('#mainmenu').css('border-bottom', '0px solid #aeaeae');
 	    $(this).css('background-color','#3A5795');
@@ -56,12 +43,7 @@ var main = function() {
 		$('#mainmenu a').css('background-color','#ffffff');
 		$('#mainmenu a').css('color','#5a5a5a');
 		$('#secmenu ul').empty();
-	    $('#menunr1').append('
-	    	<li><a href="#">Contact</a></li>
-	    	<li><a href="#">Orar</a></li>
-	    	<li><a href="#">EDU</a></li>
-	    	<li><a href="#">ISJVN</a></li>
-	    	<li><a href="#">CCDF</a></li>');
+	    $('#menunr1').append('<li><a href="#">Contact</a></li><li><a href="#">Orar</a></li><li><a href="#">EDU</a></li><li><a href="#">ISJVN</a></li><li><a href="#">CCDF</a></li>');
 	    $('#secmenu').css('border-bottom', '1px solid #aeaeae');
 	    $('#mainmenu').css('border-bottom', '0px solid #aeaeae');
 	    $(this).css('background-color','#3A5795');
